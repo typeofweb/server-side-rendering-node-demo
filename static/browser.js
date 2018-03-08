740e35c53509051f3a76cfe42a852f2c3f8bd655
Michal Miszczyszyn <michal@mmiszy.pl>
2018-03-08 10:48:34 +0100

    4 Use renderer in the browser

--- static/browser.js
+++ static/browser.js
@@ -2,6 +2,10 @@ document.body.addEventListener('click', e => {
   if (e.target.tagName.toLowerCase() === 'a') {
     e.preventDefault();
     const { href, pathname } = e.target;
+
     history.pushState(null, null, href);
+    
+    const view = window.returnExports.Renderer({originalUrl: pathname}, true);
+    document.querySelector('#view').innerHTML = view;
   }
 }, false);
